772de7ab4da36a26315d5778a807c65819629cfb
Eric Tang <67912192+EZ-Tang@users.noreply.github.com>
2022-07-11 11:08:06 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'AddOnePackage',
+    version = '0.1.0',
+    packages = ['M14Exercise'],
+    author = "Rory Black & Eric Tang",
+    author_email = 'ezt4q@virginia.edu',
+    description = 'An awesome package that does something',
+    url = 'https://github.com/EZ-Tang/Tang_Black_M14Exercise',
+    license = 'MIT'
+)
